3938e7e4f958daa54a7f46ca378dabcc2d916dfe
Mirko-Genovesi <mirkogenovesi286@gmail.com>
2024-05-02 11:06:12 -0300

    update

--- Anti-Rebote.ino
+++ Anti-Rebote.ino
@@ -1,56 +1,82 @@
+#define DELAY 40
+
 #define BUTTON_PIN 2
-#define OUTPUT_LED_PIN 3
-#define PRESS_LED_PIN 4
-#define RELEASE_LED_PIN 5
+#define PRESS_LED_PIN 3
+#define RELEASE_LED_PIN 4
 
 #define BUTTON digitalRead(BUTTON_PIN)
-#define OUT_LED digitalWrite(OUTPUT_LED_PIN, out_led_var)
-#define PR_LED digitalWrite(PRESS_LED_PIN, press_led_var)
-#define RE_LED digitalWrite(RELEASE_LED_PIN, release_led_var)
 
 
 typedef enum{
-BUTTON_UP,
-BUTTON_FALLING,
-BUTTON_DOWN,
-BUTTON_RAISING,
+    BUTTON_UP,
+    BUTTON_FALLING,
+    BUTTON_DOWN,
+    BUTTON_RAISING,
 } debounceState_t;
-
 debounceState_t current_state;
 
 
 debounceState_t debounceFSM_init(){
     current_state = BUTTON_UP;
-}		// debe cargar el estado inicial
+}
+
+
+void buttonReleased(){
+    char state = digitalRead(RELEASE_LED_PIN);
+    digitalWrite(RELEASE_LED_PIN, !state);
+}
+
+
+void buttonPressed(){
+    char state = digitalRead(PRESS_LED_PIN);
+    digitalWrite(PRESS_LED_PIN, !state);
+}
+
+
+void debounceFSM_update(){
+    switch (current_state){
+        case BUTTON_UP:
+            if (BUTTON){
+                current_state = BUTTON_FALLING;
+                time = millis();
+            }
+
+        case BUTTON_FALLING:
+            if (BUTTON){
+                if (time + DELAY >= millis()){
+                    current_state = BUTTON_DOWN;
+                    buttonPressed();
+                }
+            }
+            else current_state = BUTTON_UP;
+
+        case BUTTON_DOWN:
+            if (!BUTTON){
+                current_state = BUTTON_RAISING;
+                time = millis();
+            }
+        
+        case BUTTON_RAISING:
+            if (!BUTTON){
+                if (time + DELAY >= millis()){
+                    current_state = BUTTON_UP;
+                    buttonReleased;
+                }
+            }
+            else current_state = BUTTON_DOWN;
+    }
+}
 
 
 void setup(){
     pinMode(BUTTON_PIN, INPUT);
-    pinMode(OUTPUT_LED_PIN, OUTPUT);
     pinMode(PRESS_LED_PIN, OUTPUT);
     pinMode(RELEASE_LED_PIN, OUTPUT);
     debounceFSM_init();
+    static unsigned long time = 0;
 }
 
 
 void loop(){
-    switch (current_state){
-        case BUTTON_UP{
-            if(BUTTON)
-        }
-    }
-
+    debounceFSM_update();
 }
-
-
-
-void debounceFSM_update();	// debe leer las entradas, resolver la lógica de
-					// transición de estados y actualizar las salidas
-void buttonPressed();			// debe invertir el estado del LED1
-void buttonReleased();		// debe invertir el estado del LED2 
-
-
-
-
-
-
